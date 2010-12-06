22e83634d9afb3fee69d0662862b7332cf1c9659
fujohnwang <fujohnwang@gmail.com>
2010-12-06 14:33:45 +0800

    separate main to a standalone src file

--- src/main/scala/Hs2client.scala
+++ src/main/scala/Hs2client.scala
@@ -334,27 +334,4 @@ package hs2client{
 		}
 	}
 	
-	object SandboxRunner {
-	  def main(args: Array[String]): Unit = {
-		val sessionFactory = new Hs2SessionFactoryImpl(Hs2Conf("10.16.201.39",connectionEncoding="GBK"))
-		val session = sessionFactory.openSession(OpenIndexSpec("test", "dw", Array("id", "value")), true)
-		try{
-			println("result: "+session.get(Eq(),Array("13")).mkString(","))
-		}
-		finally{
-			session.close
-		}
-		
-		val wrsession = sessionFactory.openSession(OpenIndexSpec("test", "dw", Array("value")))
-		try{
-			wrsession.insert(Array("17", "darren wang"))
-			wrsession.update(Eq(), Array("17"), Array("fujohnwang"))
-			wrsession.delete(Eq(), Array("17"))
-		}
-		finally{
-			wrsession.close
-		}
-	  }
-	}
-	
 }
